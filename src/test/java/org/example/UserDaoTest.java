80113b9e65b13d7bf91ef039d55fdb987241dd19
yjyj1023 <lyj19961023@gmail.com>
2022-10-25 13:43:02 +0900

    deleteAll(), getCount()추가 - 166p

--- src/test/java/org/example/UserDaoTest.java
+++ src/test/java/org/example/UserDaoTest.java
@@ -10,6 +10,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import user.User;
 
 import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
@@ -21,10 +24,20 @@ class UserDaoTest {
     void addAndSelect() throws SQLException, ClassNotFoundException {
 
         UserDao userDao = new UserDaoFactory().awsUserDao();
-        User user = new User("8", "EternityHwan","1123");
+
+        //컬럼삭제
+        userDao.deleteAll();
+
+        //유저 추가
+        User user = new User("1", "EternityHwan","1123");
         userDao.add(user);
 
-        User selectedUser = userDao.get("8");
-        Assertions.assertEquals("EternityHwan", selectedUser.getName());
+        //users 레코드의 개수
+        assertEquals(1,userDao.getCount());
+
+        //id = 1인 유저의 이름과 패스워드 불러오기
+        User selectedUser = userDao.get("1");
+        assertEquals("EternityHwan", selectedUser.getName());
+        assertEquals("1123", selectedUser.getPassword());
     }
 }
