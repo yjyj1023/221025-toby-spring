553d2c43bf39cebf5a77ea530bbcb0eab8969b2a
yjyj1023 <lyj19961023@gmail.com>
2022-10-25 13:44:49 +0900

    deleteAll(), getCount() 테스트 추가

--- src/test/java/org/example/UserDaoTest.java
+++ src/test/java/org/example/UserDaoTest.java
@@ -40,4 +40,26 @@ class UserDaoTest {
         assertEquals("EternityHwan", selectedUser.getName());
         assertEquals("1123", selectedUser.getPassword());
     }
+
+    @Test
+    void count() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDaoFactory().awsUserDao();
+
+        User user1 = new User("1","박성철","1234");
+        User user2 = new User("2","이길원","1321");
+        User user3 = new User("3","박범진","4321");
+
+        userDao.deleteAll();
+        assertEquals(0,userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1,userDao.getCount());
+
+        userDao.add(user2);
+        assertEquals(2,userDao.getCount());
+
+        userDao.add(user3);
+        assertEquals(3,userDao.getCount());
+
+    }
 }
