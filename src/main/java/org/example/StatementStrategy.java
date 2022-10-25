54fa5edfce9e4604c5bbc6ad6edf1ae282f467f7
yjyj1023 <lyj19961023@gmail.com>
2022-10-25 15:16:53 +0900

    JdbcTemplate적용 getCount(), get() - 261p

--- src/main/java/org/example/StatementStrategy.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public interface StatementStrategy {
-    PreparedStatement makePreparedStatement(Connection c)throws SQLException;
-}
--- src/main/java/org/example/UserDao.java
+++ src/main/java/org/example/UserDao.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import user.User;
 
 import javax.sql.DataSource;
@@ -24,50 +25,16 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-            //db접속
-            c = dataSource.getConnection();
-
-            //sql을 담은 PreparedStatement 만들고 setString으로 값넣기
-            ps = c.prepareStatement("SELECT id,name,password FROM users WHERE id = ?");
-            ps.setString(1, id);
-
-            //조회의 경우 sql쿼리의 실행결과를 ResultSet으로 받아서 정보를 저장할 오브젝트(User)에 옮겨줌.
-            rs = ps.executeQuery();
-
-            //User는 null로 초기화
-            User user = null;
-
-            //select문의 존재여부 확인(다음 행이 존재하면 true 리턴)
-            if(rs.next()){
-                user = new User(rs.getString("id"),
-                        rs.getString("name"), rs.getString("password"));
+        String sql = "select * from users where id = ?";
+        RowMapper<User> rowMapper = new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User(rs.getString("id"), rs.getString("name"),
+                        rs.getString("password"));
+                return user;
             }
-
-            //결과가 없으면 User는 null상태 그대로 이므로 예외를 던져준다.
-            if(user == null) throw new EmptyResultDataAccessException(1);
-            return user;
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                rs.close();
-            } catch (SQLException e) {
-            }
-            try {
-                ps.close();
-            } catch (SQLException e) {
-            }
-            try {
-                c.close();
-            } catch (SQLException e) {
-            }
-        }
+        };
+        return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
 
     public void deleteAll() throws SQLException{
@@ -75,62 +42,8 @@ public class UserDao {
     }
 
     public int getCount() throws SQLException, ClassNotFoundException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("select count(*) from users");
-
-            rs = ps.executeQuery();
-            rs.next();
-
-            return rs.getInt(1);
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                rs.close();
-            } catch (SQLException e) {
-            }
-            try {
-                ps.close();
-            } catch (SQLException e) {
-            }
-            try {
-                c.close();
-            } catch (SQLException e) {
-            }
-        }
-    }
-
-    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException{
-        Connection c = null;
-        PreparedStatement ps = null;
-        try {
-            c = dataSource.getConnection();
-            ps = stmt.makePreparedStatement(c);
-
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if(ps != null){
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-
-            if(c != null){
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
+        //queryForObject에 두번째 파라메터로 Integer.class를 넘겨줌으로써 int형의 데이터를 받아온다.
+        return this.jdbcTemplate.queryForObject("select count(*) from users;", Integer.class);
     }
 
 }
