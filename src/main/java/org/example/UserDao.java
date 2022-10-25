684ce51ebf0568d25c4168c4660d79c232dc0dfb
yjyj1023 <lyj19961023@gmail.com>
2022-10-25 14:05:38 +0900

    try / catch / final 처리 - 212p

--- src/main/java/org/example/UserDao.java
+++ src/main/java/org/example/UserDao.java
@@ -14,50 +14,85 @@ public class UserDao {
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = null;
+        PreparedStatement ps = null;
 
-        //sql을 담은 PreparedStatement 만들고 setString으로 값넣기
-        PreparedStatement ps = c.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?)");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
+        try {
+            //db접속
+            c = connectionMaker.makeConnection();
 
-        //만들어진 PreparedStatement 실행하기
-        ps.executeUpdate();
+            //sql을 담은 PreparedStatement 만들고 setString으로 값넣기
+            ps = c.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?)");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
 
-        //리소스 닫기
-        ps.close();
-        c.close();
-        System.out.println("DB연동 성공");
+            //만들어진 PreparedStatement 실행하기
+            ps.executeUpdate();
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                ps.close();
+            } catch (SQLException e) {
+            }
+            try {
+                c.close();
+            } catch (SQLException e) {
+            }
+        }
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
-        //sql을 담은 PreparedStatement 만들고 setString으로 값넣기
-        PreparedStatement ps = c.prepareStatement("SELECT id,name,password FROM users WHERE id = ?");
-        ps.setString(1, id);
+        try {
+            //db접속
+            c = connectionMaker.makeConnection();
 
-        //조회의 경우 sql쿼리의 실행결과를 ResultSet으로 받아서 정보를 저장할 오브젝트(User)에 옮겨쥼.
-        ResultSet rs = ps.executeQuery();
+            //sql을 담은 PreparedStatement 만들고 setString으로 값넣기
+            ps = c.prepareStatement("SELECT id,name,password FROM users WHERE id = ?");
+            ps.setString(1, id);
 
-        //User는 null로 초기화
-        User user = null;
+            //조회의 경우 sql쿼리의 실행결과를 ResultSet으로 받아서 정보를 저장할 오브젝트(User)에 옮겨줌.
+            rs = ps.executeQuery();
 
-        //select문의 결과가 있으면(다음 행이 존재하면) true리턴
-        if (rs.next()) {
-            user = new User(rs.getString("id"),
-                    rs.getString("name"), rs.getString("password"));
+            //User는 null로 초기화
+            User user = null;
 
+            //select문의 존재여부 확인(다음 행이 존재하면 true 리턴)
+            if(rs.next()){
+                user = new User(rs.getString("id"),
+                        rs.getString("name"), rs.getString("password"));
+            }
+
+            //결과가 없으면 User는 null상태 그대로 이므로 예외를 던져준다.
+            if(user == null) throw new EmptyResultDataAccessException(1);
+            return user;
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+            }
+            try {
+                ps.close();
+            } catch (SQLException e) {
+            }
+            try {
+                c.close();
+            } catch (SQLException e) {
+            }
         }
-        //리소스 닫기
-        rs.close();
-        ps.close();
-        c.close();
-
-        //결과가 없으면 User는 null상태 그대로 이므로 예외를 던져준다.
-        if (user == null) throw new EmptyResultDataAccessException(1);
-        return user;
     }
 
     public void deleteAll() throws SQLException, ClassNotFoundException {
